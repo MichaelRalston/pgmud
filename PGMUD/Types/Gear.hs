fb38d235a45998bba22bbef57d6f80377e9cb9f1
Michael Ralston <michael.ralston@gmail.com>
2016-12-27 23:26:25 -0800

    Add weapon groups and a function to check if a weapon is in a group.

--- PGMUD/Types/Gear.hs
+++ PGMUD/Types/Gear.hs
@@ -3,12 +3,15 @@
 module PGMUD.Types.Gear
     ( WeaponClass (..)
     , ItemLevel (..)
+    , WeaponGroup (..)
+    , isInGroup
     ) where
     
 import PGMUD.Prelude
 import Data.Csv (FromField(..))
     
 data WeaponClass = Staff | Book | Fist | Axe | Orb | Wand | Rapier | Spear | Bow | Dagger | Whip | TwoHanded | Thrown deriving (Eq, Ord, Bounded, Enum, Show)
+data WeaponGroup = Mystical | Large | Bladed | OneHanded | Ranged | Tricky deriving (Eq, Ord, Bounded, Enum, Show)
 newtype ItemLevel = ItemLevel Int deriving (Show, Num, Eq, Ord) -- obviously there needs to be data here, but ...
 
 instance Nameable WeaponClass where
@@ -26,5 +29,35 @@ instance Nameable WeaponClass where
     name TwoHanded = "twohanded"
     name Thrown = "thrown"
 
+instance Nameable WeaponGroup where
+    name Mystical = "mystical"
+    name Large = "large"
+    name Bladed = "bladed"
+    name OneHanded = "onehanded"
+    name Ranged = "ranged"
+    name Tricky = "tricky"
+    
+isInGroup :: WeaponClass -> WeaponGroup -> Bool
+isInGroup Staff Mystical = True
+isInGroup Staff Large = True
+isInGroup Book Mystical = True
+isInGroup Fist Tricky = True
+isInGroup Axe Bladed = True
+isInGroup Orb Mystical = True
+isInGroup Orb OneHanded = True
+isInGroup Wand Mystical = True
+isInGroup Wand OneHanded = True
+isInGroup Rapier OneHanded = True
+isInGroup Rapier Tricky = True
+isInGroup Spear Large = True
+isInGroup Bow Ranged = True
+isInGroup Dagger OneHanded = True
+isInGroup Dagger Bladed = True
+isInGroup Whip Tricky = True
+isInGroup TwoHanded Large = True
+isInGroup TwoHanded Bladed = True
+isInGroup Thrown Ranged = True
+isInGroup _ _ = False
+    
 instance FromField ItemLevel where
     parseField f = ItemLevel <$> parseField f
